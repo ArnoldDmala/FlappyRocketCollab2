d74c014b36a1e804c0858f2d241ad36dacc90fde
Arnold Dmala <arnoldouangmala@gmail.com>
2018-08-30 16:10:45 -0400

    Adding Level Music
    NOW WORKS!!!

---
+++ Assets/DontDestroyAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyAudio : MonoBehaviour {
+
+    static bool AudioBegin = false;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+     void Update()
+    {
+        if(Application.loadedLevelName == "MainMenu")
+        {
+            audioSource.Stop();
+            AudioBegin = false;
+        }
+    }
+}
